ad10ec50f469315abe78e46bb8ea94ccbe0742c9
SEB <sebbanholmer@gmail.com>
2017-05-02 13:21:43 +0200

    Button Design and ...

--- index.php
+++ index.php
@@ -170,7 +170,7 @@ OUT;
 ?>
                 <!-- Page changer -->
 
-                <button tupe="button" class="readMore"><span class="glyphicon glyphicon-chevron-down randomAlign"></span></button>
+                <button tupe="button" class="readMore"><a herf="#"><span class="glyphicon glyphicon-chevron-down randomAlign"></span></a></button>
 
               <!--<ul class="pager">
                     <li class="previous">
@@ -238,6 +238,15 @@ OUT;
                             </div>
                             <div class="g-follow" data-href="https://plus.google.com/u/0/113858997105593337196" data-rel="{relationshipType}"></div>
                     </section>
+                </div>
+                <div class="well">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident,
+                   sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
             </div>
         </div>
